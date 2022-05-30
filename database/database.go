03c13725137056be269d1a9c857cc3dc771848aa
lindsaymb15 <lmorales@akurey.com>
2022-05-30 12:26:26 -0600

    [fix] Updated env variables names

--- database/database.go
+++ database/database.go
@@ -12,11 +12,11 @@ import (
 var DB *gorm.DB
 
 var (
-	DB_HOST     = os.Getenv("HOST")
-	DB_PORT     = os.Getenv("PORT")
-	DB_USER     = os.Getenv("USR")
-	DB_PASSWORD = os.Getenv("PASS")
-	DB_NAME     = os.Getenv("DATABASE")
+	DB_HOST     = os.Getenv("DB_HOST")
+	DB_PORT     = os.Getenv("DB_PORT")
+	DB_USER     = os.Getenv("DB_USER")
+	DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	DB_NAME     = os.Getenv("DB_NAME")
 )
 
 func InitDatabase() {
