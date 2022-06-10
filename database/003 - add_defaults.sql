89d8685904ae075f8f72630e581c6f3ad1321156
enavarroAK <86040286+enavarroAK@users.noreply.github.com>
2022-06-10 12:53:13 -0600

    [fix][ABE-21] Added missing default values to AP_Conservation_Status and AP_Animal_Classification tables

--- database/003 - add_defaults.sql
+++ database/003 - add_defaults.sql
@@ -1,7 +1,7 @@
 ALTER TABLE "AP_Conservation_Status" 
-ALTER COLUMN "created_at" timestamp DEFAULT NOW(),
-ALTER COLUMN "updated_at" timestamp DEFAULT NOW(),
-ALTER COLUMN "is_deleted" bit DEFAULT '0'
+ALTER COLUMN "created_at" SET DEFAULT NOW(),
+ALTER COLUMN "updated_at" SET DEFAULT NOW(),
+ALTER COLUMN "is_deleted" SET DEFAULT '0'
 
 ALTER TABLE "AP_Animal_Classification" 
 ALTER COLUMN "created_at" SET DEFAULT NOW(),
