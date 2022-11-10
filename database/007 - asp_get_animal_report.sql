8a96f67936813ae4d80811d4d3ffc7c3776fa236
Rodrigo <75322979+rnavarroSF@users.noreply.github.com>
2022-11-10 11:33:09 -0600

    [ABE-36] - ASP_GetAnimalReport (#47)
    * Added ASP_GetAnimalReport

---
+++ database/007 - asp_get_animal_report.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION CAST_TO_INT(TEXT, INTEGER) RETURNS INTEGER
+AS $$
+BEGIN
+    RETURN CAST($1 AS INTEGER);
+    EXCEPTION
+    	WHEN invalid_text_representation THEN
+        	RETURN $2;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION AFN_GetAnimalReport(pReportId int)
+RETURNS SETOF record
+      LANGUAGE 'plpgsql'
+      AS $BODY$
+BEGIN
+RETURN QUERY
+
+    WITH Directions AS (
+        SELECT D.id, JSONB_BUILD_OBJECT('Provincia', AP.name, 'Canton', AC.name, 'Distrito', D.name) AS value
+        FROM "AP_Districts" D
+        INNER JOIN "AP_Cantons" AC ON AC.id = D.canton_id
+        INNER JOIN "AP_Provinces" AP ON AP.id = AC.province_id
+    )
+    SELECT AAR.id ID, AA.id IdAnimal, AA.name AnimalName, AA.scientific_name ScientificName,
+       ACS."name" ConservationStatusName, ACS.abbreviation Abbreviaton, AAC.name ClassificationName,
+       JSONB_OBJECT_AGG(AF.name, COALESCE((D.value || JSONB_BUILD_OBJECT('Exacta', SPLIT_PART(ARFV.value, '▽', 1)))::TEXT, ARFV.value)) Fields
+    FROM "AP_Animal_Reports" AAR
+         INNER JOIN "AP_Animals" AA ON AAR.animal_id = AA.id
+         INNER JOIN "AP_Conservation_Status" ACS ON AA.conservation_status_id = ACS.id
+         INNER JOIN "AP_Animal_Classification" AAC ON AA.classification_id = AAC.id
+         INNER JOIN "AP_Report_Field_Values" ARFV ON AAR.id = ARFV.report_id
+         INNER JOIN "AP_Fields" AF ON AF.id = ARFV.field_id
+         INNER JOIN "AP_Field_Types" AFT ON AF.field_type_id = AFT.id
+         LEFT JOIN Directions D ON AFT.name = 'Address' AND D.id = CAST_TO_INT(SPLIT_PART(ARFV.value, '▽', 2), 0)
+    WHERE AAR.id = pReportId
+    GROUP BY AAR.id, AA.id, AA.name, AA.scientific_name, ACS.name, ACS.abbreviation, AAC.name;
+
+END;
+$BODY$;
+
+-- Usage:
+-- SELECT * FROM public.AFN_GetAnimalReport(2) AS r(ID BIGINT, IdAnimal BIGINT, AnimalName VARCHAR, ScientificName VARCHAR, ConservationStatusName VARCHAR, Abbreviaton VARCHAR, ClassificationName VARCHAR, Fields JSONB);
