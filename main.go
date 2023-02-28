feadf22b63f850702227ba97716d599f5c2a0769
Emmanuel Murillo Sánchez <5572221+emmurillo@users.noreply.github.com>
2023-02-28 14:15:15 -0600

    feat: deploy to heroku (#54)
    * feat: deploy to heroku
    
    * feat: CORS headers
    
    * Render needed docs (#64)
    
    * [feat/deploy-to-heroku][test] Test without vendors
    
    * [Fix] Delete unused files
    
    * Added deployment info in readme
    
    ---------
    
    Co-authored-by: Emmanuel Murillo <emurillo@akurey.com>
    Co-authored-by: Esteban Jimenez <88204114+ejimenezak@users.noreply.github.com>
    Co-authored-by: ejimenezak <ejimenez@akurey.com>

--- main.go
+++ main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"animal-rescue-be/controllers"
 	"animal-rescue-be/database"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
 )
+
+func InitServerHeaders() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json")
+		c.Next()
+	}
+}
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 
 	engine := gin.Default()
+
+	engine.Use(InitServerHeaders())
 
 	animal := new(controllers.AnimalController)
 
@@ -51,14 +65,22 @@ func setupRouter() *gin.Engine {
 }
 
 func init() {
-	if godotenv.Load(".env") != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("APP_ENV") != "production" {
+		if godotenv.Load(".env") != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 }
 
 func main() {
 	database.InitDatabase()
 	router := setupRouter()
+	port := "8080"
+
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+
 	// Listen and Server in 0.0.0.0:8080
-	router.Run(":8080")
+	router.Run(fmt.Sprintf(":%v", port))
 }
